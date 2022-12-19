69ec47623e469f36bcb32c6d022c5ccd9ed65f16
khalid <khalidmamdou7@gmail.com>
2022-12-19 15:15:33 +0200

    process-generator: refactor reading process from inputfile

--- process_generator.c
+++ process_generator.c
@@ -6,35 +6,16 @@
 
 void clearResources(int);
 
+void readProcessFileAndEnqueue(char* filePath, struct Queue* q);
+
 int main(int argc, char *argv[])
 {
     // signal(SIGINT, clearResources);
     // TODO Initialization
 
     // 1. Read the input files.
-    char* filePath = argv[1];
-    FILE* inpFilePtr = fopen(filePath, "r");
-
-    if (inpFilePtr == NULL)
-    {
-        printf("Error opening file");
-        exit(1);
-    }
-
     struct Queue* q = createQueue();
-
-    char buff[120];
-    while (fgets(buff, 120, inpFilePtr))//problem in reading process 5
-    {
-        if(buff[0]=='#')//skip comment
-            continue;
-        
-        struct ProcessData p;
-        sscanf(buff, "%d %d %d %d", &p.id, &p.arrivalTime, &p.runningTime, &p.priority);
-        enqueue(q, p);
-    }
-    printQueue(q);
-    fclose(inpFilePtr);
+    readProcessFileAndEnqueue(argv[1], q);
 
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     if (argc < 4)
@@ -109,4 +90,30 @@ void clearResources(int signum)
     //TODO Clears all resources in case of interruption
     destroyMsgQueue(ftok("keyfile", 65));
     destroyClk(true);
+}
+
+
+void readProcessFileAndEnqueue(char* filePath, struct Queue* q)
+{
+    FILE* inpFilePtr = fopen(filePath, "r");
+
+    if (inpFilePtr == NULL)
+    {
+        printf("Error opening file");
+        exit(1);
+    }
+
+    char buff[120];
+    while (fgets(buff, 120, inpFilePtr))//problem in reading process 5
+    {
+        if(buff[0]=='#')//skip comment
+            continue;
+        
+        struct ProcessData p;
+        sscanf(buff, "%d %d %d %d", &p.id, &p.arrivalTime, &p.runningTime, &p.priority);
+        enqueue(q, p);
+    }
+    printQueue(q);
+    fclose(inpFilePtr);
+
 }
