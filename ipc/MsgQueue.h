8c7edc5e4dc5bf64058765498d0ecf451b80441b
khalid <khalidmamdou7@gmail.com>
2022-12-19 11:48:48 +0200

    ipc-classes: msg queue

---
+++ ipc/MsgQueue.h
@@ -0,0 +1,16 @@
+#ifndef MSGQUEUE_H
+#define MSGQUEUE_H
+
+#include <sys/ipc.h>
+#include "MsgStruct.h"
+
+
+
+int getMsgQueue(key_t key);
+void sendMsg(int msgid, struct MsgStruct msg);
+struct MsgStruct receiveMsg(int msgid, long mtype);
+void destroyMsgQueue(int msgid);
+
+
+
+#endif
